9fee31ecc71d0377b183ba11be3f7dd4e5b0ddbd
devson <sonyc5720@gmail.com>
2018-09-07 22:01:23 +0900

    GreetingClient 변경 및 테스트

--- src/main/java/com/simple/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/simple/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,7 @@
 package com.simple.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -13,9 +14,29 @@ public class GreetingClient {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8888)
                                                     .usePlaintext() // security (SSL)
                                                     .build();
+
+        // created a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetSyncClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                                        .setFirstName("Chris")
+                                        .setLastName("Son")
+                                        .build();
+
+        // create greeting request
+        GreetRequest request = GreetRequest.newBuilder()
+                                            .setGreeting(greeting)
+                                            .build();
+
+        // call the RPC and get back a GreetResponse (protocol buffer)
+        GreetResponse response = greetSyncClient.greet(request);
+
+        System.out.println(response.getResult());
+        // old & dummy
         // blocking stub = synchronous
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
-        System.out.println("BlockingStub was created");
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+        // System.out.println("BlockingStub was created");
 
         // future stub = asynchronous
         // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
@@ -24,5 +45,4 @@ public class GreetingClient {
         System.out.println("Shutting down channel");
         channel.shutdown();
     }
-
 }
